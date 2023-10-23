4101be811137b6303df8265fd68b83f7e6f8f14d
Luke G <itzlukeg14@gmail.com>
2023-10-23 16:18:27 +0000

    platform start

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -28,12 +28,12 @@ $(function () {
      * Comment the lines out to remove the grid
      */
 
-    // for (let i = 100; i < canvas.width; i += 100) {
-    //   createPlatform(i, canvas.height, -1, -canvas.height);
-    // }
-    // for (let i = 100; i < canvas.height; i += 100) {
-    //   createPlatform(canvas.width, i, -canvas.width, -1);
-    // }
+     for (let i = 100; i < canvas.width; i += 100) {
+       createPlatform(i, canvas.height, -1, -canvas.height);
+     }
+     for (let i = 100; i < canvas.height; i += 100) {
+       createPlatform(canvas.width, i, -canvas.width, -1);
+     }
 
     /////////////////////////////////////////////////
     //////////ONLY CHANGE BELOW THIS POINT///////////
@@ -43,7 +43,18 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
+    createPlatform(200, 700, 100, 5);
+    createPlatform(200, 500, 100, 5);
+    createPlatform(200, 300, 100, 5);
+    createPlatform(200, 100, 100, 5);
+    
+    createPlatform(400, 600, 100, 5);
+    createPlatform(400, 400, 100, 5);
+    createPlatform(400, 200, 100, 5);
 
+    
+    createPlatform(200, 0, 1, 500);
+    
 
 
     
@@ -68,6 +79,7 @@ $(function () {
     /////////////////////////////////////////////////
     //////////ONLY CHANGE ABOVE THIS POINT///////////
     /////////////////////////////////////////////////
+    
   }
 
   registerSetup(setup);
