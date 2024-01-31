43742f86cf735f5646e7e6128f89f9c1949cafc1
Luke G <itzlukeg14@gmail.com>
2024-01-31 17:29:39 +0000

    ToPart4

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -59,11 +59,16 @@ else if (currentShape.color === 'blue') {
 else {currentShape.goodBehavior = 'spin'}
 }
   // TODO 3-a: add a function that handles the static display type
-
-  
+function handleStatic(data) {
+  setBackgroundWithObject(data);
+  animationDetails.displayType = 1;
+}
 
   // TODO 4-a: add a function that handles the good display type
-  
+  function handleGood(color, shape, repeat){
+  setBackgroundWithSimple(color, shape, repeat);
+  animationDetails.displayType = 2;
+  }
 
   // TODO 5-a: add a function that handles the bad display type
   
@@ -74,12 +79,13 @@ else {currentShape.goodBehavior = 'spin'}
 
   function staticDisplay() {
     // TODO 3-b: call your handleStatic function
-    
+    handleStatic(dataShapes[currentIndex])
   }
 
   function goodDisplay() {
     // TODO 4-b: call your handleGood function
-    
+    var currentShape = dataShapes[currentIndex];
+    handleGood(currentShape.color, currentShape.shape, currentShape.repeat)
   }
 
   function badDisplay() {
