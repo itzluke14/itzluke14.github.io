7e10811dc62351e37bf035f74c4045b6e83f2bd5
Luke G <itzlukeg14@gmail.com>
2024-02-05 16:49:42 +0000

    doesthiswork

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -71,7 +71,11 @@ function handleStatic(data) {
   }
 
   // TODO 5-a: add a function that handles the bad display type
-  
+  function handleBad(data, repeat){
+    repeat += 1;
+    setBackgroundWithMixed(data, repeat);
+    animationDetails.displayType = 3;
+  }
 
   /////////////////////////////////////////////////
   // BUTTON HANDLERS BELOW HERE (3-b, 4-b, 5-b) ///
@@ -90,7 +94,9 @@ function handleStatic(data) {
 
   function badDisplay() {
     // TODO 5-b: call your handleBad function
-    
+    var currentShape = dataShapes[currentIndex];
+    var repeat = currentShape.repeat;
+    handleBad(currentShape, repeat)
   }
 
   /////////////////////////////////////////////////
