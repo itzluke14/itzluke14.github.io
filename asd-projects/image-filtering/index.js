8b608cbaefed4a3d6e69bdd43fbd63ae6ebea5be
Luke G <itzlukeg14@gmail.com>
2024-04-16 16:00:39 +0000

    saving image filtering

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -6,9 +6,11 @@ $(document).ready(function () {
   $("#reset").on("click", resetAndRender);
 });
 
+
 /////////////////////////////////////////////////////////
 //////// event handler functions are below here /////////
 /////////////////////////////////////////////////////////
+
 
 // this function resets the image to its original value; do not change this function
 function resetAndRender() {
@@ -16,34 +18,89 @@ function resetAndRender() {
   render($("#display"), image);
 }
 
+
 // this function applies the filters to the image and is where you should call
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
+  // applyFilter(reddify)
+  // applyFilter(decreaseBlue)
+  // applyFilter(increaseGreenByBlue)
 
-  
+  applyFilterNoBackground(reddify)
+  applyFilterNoBackground(decreaseBlue)
+  applyFilterNoBackground(increaseGreenByBlue)
 
   // do not change the below line of code
   render($("#display"), image);
 }
 
+
 /////////////////////////////////////////////////////////
 // "apply" and "filter" functions should go below here //
 /////////////////////////////////////////////////////////
 
+
 // TODO 1, 2 & 4: Create the applyFilter function here
+function applyFilter(filterFunction) {
+  for (let row = 0; row < image.length; row++) {
+    for (let column = 0; column < image[row].length; column++) {
+      var rgbString = image[row][column]
+      var rgbNumbers = rgbStringToArray(rgbString)
+
+      filterFunction(rgbNumbers)
+
+      rgbString = rgbArrayToString(rgbNumbers);
+      image[row][column] = rgbString;
+
+
+    }
+  }
+}
 
 
 // TODO 7: Create the applyFilterNoBackground function
+function applyFilterNoBackground(filterFunction) {
+  var bgColor = image[0][0]
+  for (let row = 0; row < image.length; row++) {
+    for (let column = 0; column < image[row].length; column++) {
+      var rgbString = image[row][column]
+      var rgbNumbers = rgbStringToArray(rgbString)
+
+      if (rgbString !== bgColor) {
+        filterFunction(rgbNumbers)
+
+        rgbString = rgbArrayToString(rgbNumbers);
+        image[row][column] = rgbString;
+      }
+    }
+  }
+}
+
 
 
 // TODO 5: Create the keepInBounds function
-
+function keepInBounds(boundNumber) {
+  return Math.min(255, Math.max(0, boundNumber))
+}
 
 // TODO 3: Create reddify function
-
+function reddify(rgbArray) {
+  rgbArray[RED] = 200
+}
 
 // TODO 6: Create more filter functions
+function decreaseBlue(rgbArray) {
+  rgbArray[BLUE] = keepInBounds(rgbArray[BLUE] - 50)
+}
 
+function increaseGreenByBlue(rgbArray) {
+  rgbArray[GREEN] = keepInBounds(rgbArray[BLUE] + rgbArray[GREEN])
+}
 
 // CHALLENGE code goes below here
+
+
+
+
+
